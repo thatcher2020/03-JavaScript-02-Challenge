3750a05588be6b4d4da75bb2a8552690ba085022
Joseph Thatcher <thatcherjoe20@gmail.com>
2023-03-02 14:54:32 -0500

    not working yet

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-// Assignment Code 
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowercase = "abcdefghijklmnopqrstuvwxyz"
+var uppercase = lowercase.toLocaleUpperCase
+var special = "!@#$%"
+var numeric = "123456789"
 
 // Write password to the #password input
 function writePassword() {
@@ -9,6 +13,46 @@ function writePassword() {
   passwordText.value = password;
 
 }
+// only works if add return
+function generatePassword() {
+  var finalpwd = ""
+  var size = prompt("enter 8 to 128 characters")
+  
 
+
+  // then do validations, if i am doing checking
+
+  if (size >= 8 && size <= 128) {
+    var isLowercase = confirm("include lowercase?")
+    console.log(isLowercase)
+
+    var isUppercase = confirm("include uppercase?")
+    console.log(isUppercase)
+
+    var isSpecial = confirm("include special character?")
+    console.log(isSpecial)
+
+    var isNumeric = confirm("include numeric?")
+    console.log(isNumeric)
+
+    if (lowercase === true) {
+      var PositionString = Math.floor(math.random(35) * 26)
+      console.log(PositionString)
+      finalpwd = finalpdw + lowercase[PositionString]
+      console.log(finalpwd)
+    }
+// use a let to define display?
+    function display(display) {
+      var passwordText = document.querySelector("#password");
+      passwordText.value = display;
+      return display;
+    }
+  } 
+  else {
+    alert("invalid entry")
+  }
+
+  return finalpwd
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
